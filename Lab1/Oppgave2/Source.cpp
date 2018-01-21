dc2162da4cde3eec244a57e53f3661ea02764d9c
Andrew Jianyou Day <andrewj.day@hotmail.com>
2018-01-21 21:20:17 +0100

    ferdig oppgave2

--- Lab1/Oppgave2/Source.cpp
+++ Lab1/Oppgave2/Source.cpp
@@ -1,10 +1,46 @@
+/*
+Oppgave 2
+
+Du skal lage en funksjon som får et heltall som parameter og returnerer fakultetet av tallet. 
+Bruk datatypen long long. Ettersom jeg ikke har vist funksjoner enda, 
+gis et skall du kan fylle inn i. 
+Skriv en applikasjon som leser inn n og skriver ut n! (n fakultet), n er en long.
+
+long fakultet (long n) {
+// fyll inn kode for å beregne og returnere n fakultet
+}
+
+*/
+
 #include <iostream>
 using namespace std;
 
+int number;
+int factorial = 1;
+
+
+long fakultet(long n) {
+	
+	for (int i = n; i >= 1; i--) {
+
+		factorial = factorial * i;
+	}
+
+	return factorial;
+}
+
+
+
 int main() {
 
-	cout << "Hello World" << endl;
+	cout << "Enter a number > " << flush;
+	cin >> number;
 
-	cin.get();
+	fakultet(number);
+
+	cout << "The factorial of " << number << " is: " << factorial << endl;
+
+
+	system("pause");
 	return 0;
 }
