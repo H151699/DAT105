dd7d2c2424f36bb30755ddefcfc5a896f80668e6
Andrew Jianyou Day <andrewj.day@hotmail.com>
2018-01-21 20:44:12 +0100

    Ferdig oppgave1

---
+++ Lab1/Oppgave1/Heidu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	string inputName; 
+	cin >> inputName; // leser inn input
+	
+
+	cout << "Hello "  << inputName << "!" << endl;
+	
+
+	system("pause");
+	return 0;
+}
